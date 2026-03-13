661aaa22d18680551b0eb086dea88c55d9dff793
Arthur Manenti <arthur.manenti.videira@gmail.com>
2026-03-12 21:55:11 -0300

    Entendendo como criar e usar componentes

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
+  <TheHeader />
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue';
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    TheHeader
   }
 }
 </script>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,13 @@
+<template>
+    <header class="header">Header</header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.header {
+    background-color: aqua;
+}
+</style>
