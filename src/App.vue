6d03e83925f5d23e62109fd1c0a1889445101e32
Arthur Manenti <arthur.manenti.videira@gmail.com>
2026-03-13 16:26:59 -0300

    Two-Way Data Binding

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,61 @@
 <template>
   <div>
+    <div>
+      One Way Data Binding <br />
+      Two Way Data Binding <br />
+      v-model -> Formularios
+    </div>
 
-    <h1 :class="{ 'title': true, 'title-home': isHome }">Curso Vue 3</h1>
+    <br /><br />
 
-    <p :class="pClass">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
-      eveniet rerum iste quidem repellat non, nisi
-      alias reprehenderit veritatis debitis hic aperiam est rem nesciunt pariatur soluta sapiente laboriosam dolorum!
-    </p>
+    <div>
+      <label>Nome</label><br />
+      <input v-model="name" type="text" /><br />
+      {{ name }}
+    </div>
 
-    <p :style="styleClass">Lorem ipsum dolor sit amet consectetur, adipisicing
-      elit. Dolores autem, ex sit quo
-      exercitationem necessitatibus nostrum consequatur harum quidem, libero minus quas magni debitis sed nobis nam
-      quam? Neque, deserunt!</p>
+    <br><br>
 
-    <div class="todos-item" v-for="(obj, index) in todos" :key="obj.id">
-      <img v-if="obj.imgSrc" :src="obj.imgSrc" />
-      {{ index + 1 }} - {{ obj.title }}
+    <div>
+      <label>Esportes</label><br>
+      <select v-model="sports">
+        <option value="">Escolha</option>
+        <option value="futebol">Futebol</option>
+        <option value="skate">Skate</option>
+        <option value="tênis">Tênis</option>
+      </select>
+      <br>
+      {{ sports }}
+    </div>
+
+    <br><br>
+
+    <div>
+      <label>NewsLetter</label> <br>
+      <input v-model="newsletter" type="radio" value="Sim"> Sim <br>
+      <input v-model="newsletter" type="radio" value="Não"> Não
+      <br>
+      {{ newsletter }}
+    </div>
+
+    <br><br>
+
+    <div>
+      <label>Contrato</label> <br>
+      <input v-model="contract" type="checkbox" value="Sim"> Aceita nossos termos? <br>
+      <br>
+      {{ contract }}
+    </div>
+
+    <br><br>
+
+    <div>
+      <label>Marque as cores que você mais gosta: </label> <br>
+      <input v-model="colors" type="checkbox" value="azul"> Azul <br>
+      <input v-model="colors" type="checkbox" value="amarelo"> Amarelo <br>
+      <input v-model="colors" type="checkbox" value="vermelho"> vermelho
+      <br>
+      {{colors}}
     </div>
 
   </div>
@@ -26,46 +66,11 @@ export default {
   name: 'App',
   data() {
     return {
-      classVar: "title",
-      isHome: true,
-      pClass: ['text', 'text-home'],
-      styleClass: { color: 'aqua', backgroundColor: 'black', fontSize: '20px' },
-      todos: [
-
-        {
-          "userId": 1,
-          "id": 1,
-          "title": "delectus aut autem",
-          "completed": false,
-          "imgSrc": "https://github.com/noname697.png",
-        },
-        {
-          "userId": 1,
-          "id": 2,
-          "title": "quis ut nam facilis et officia qui",
-          "completed": false,
-          "imgSrc": "https://github.com/noname697.png",
-        },
-        {
-          "userId": 1,
-          "id": 3,
-          "title": "fugiat veniam minus",
-          "completed": false
-        },
-        {
-          "userId": 1,
-          "id": 4,
-          "title": "et porro tempora",
-          "completed": true
-        },
-        {
-          "userId": 1,
-          "id": 5,
-          "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
-          "completed": false
-        }
-
-      ]
+      name: 'jon Snow',
+      sports: '',
+      newsletter: '',
+      contract: false,
+      colors: []
     }
   }
 }
@@ -78,30 +83,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   margin: 60px;
-}
-
-.todos-item {
-  background-color: black;
-  margin: 0 0 5px 0;
-  padding: 3px 6px;
-  color: white;
-}
-
-.title {
-  font-size: 20px;
-  color: blue;
-}
-
-.title-home {
-  font-size: 40px;
-  color: green;
-}
-
-.text {
-  color: red;
-}
-
-.text-home {
-  color: #42b983;
 }
 </style>
